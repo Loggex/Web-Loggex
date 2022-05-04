3535ea4df045511a02410f52c16535563c8fe682
André Augusto Dos Santos Lima <47926331848@info.scs>
2022-05-04 11:08:50 -0300

    Login quase terminado

--- src/pages/Login/App.jsx
+++ src/pages/Login/App.jsx
@@ -3,27 +3,64 @@ import logo from '../../assets/logo.svg'
 import password from '../../assets/password.svg'
 import emailImg from '../../assets/emailImg.svg'
 import React, {useState} from 'react';
+import axios from 'axios';
+import api from "../../services/api";
+import { parseJwt } from "../../services/auth";
+import { useHistory } from "react-router-dom";
 
 
 
-export default function App() {
+export default function Login() {
   
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   
-//   realizarLogin = async() => {
-//     const resposta = await api.post('/login', {
-//         email: email,
-//         senha: senha,
-//     });
-//     const token = resposta.data.token;
+  const [isLoading, setIsLoading] = useState(false);
+    const [erroMensagem, setErroMensagem] = useState('');
 
-//     await AsyncStorage.setItem('userToken', token)
+    let history = useHistory();
 
-//     if (resposta.status == 200) {
-//         navigation.navigate('Main')
-//     }
-// };
+    function efetuarLogin(event) {
+
+        event.preventDefault();
+
+        setErroMensagem('')
+        setIsLoading(true)
+
+        api.post('/Login', {
+            email: email,
+            senha: senha
+        })
+
+            .then((response) => {
+                if (response.status === 200) {
+                    localStorage.setItem('usuario-login', response.data.token)
+
+                    setSenha('')
+
+                    setEmail('')
+
+                    setIsLoading(false)
+
+                    if (parseJwt().role === "1") {
+                        history.push('/Veiculos')
+                    }
+                    if (parseJwt().role === "2") {
+                        history.push('/Veiculos')
+                    }
+                    
+                }
+            })
+            .catch(erro => {
+                console.log(erro)
+
+                // setSenha('')
+
+                setErroMensagem("E-mail e/ou Senha inválidos")
+
+                setIsLoading(false)
+            })
+    }
 
   return (
     <div className="App">
@@ -31,7 +68,7 @@ export default function App() {
         <div className="cardLogin">
           <img src={logo} alt="Logo do loggex"className="logoLogin"/>
           <h1>Acesse sua conta</h1>
-          <form className="formLogin">
+          <form className="formLogin" onSubmit={efetuarLogin}>
             <div className="campoLogin">
               <img src={emailImg} alt='Ilustração de email'></img>
               <input className="inputLogin" type="email"placeholder="Email" value={email}  onChange={(e) => setEmail(e.target.value)}></input>
@@ -44,14 +81,14 @@ export default function App() {
             <div className="esqueceste">
               <a href="">Esqueceu sua senha?</a>
             </div>
-            <a href="/veiculos">
-            <button className='botao'>Entrar</button>
-            </a>
+            <button disabled={
+                                        email === '' || senha === ''
+                                            ? 'none'
+                                            : ''
+                                    } type="submit" className='botao'>Entrar</button>
           </form>
         </div>
       </div>
     </div>
   );
 }
-// console.log(email);
-// export default App;
