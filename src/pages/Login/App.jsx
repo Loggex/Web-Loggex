67b6a7be33ca1608e21ef0efd9f3bc9464d8be1f
André Augusto Dos Santos Lima <47926331848@info.scs>
2022-05-05 09:54:33 -0300

    Login com erro 404

--- src/pages/Login/App.jsx
+++ src/pages/Login/App.jsx
@@ -2,7 +2,7 @@ import '../../assets/App.css';
 import logo from '../../assets/logo.svg'
 import password from '../../assets/password.svg'
 import emailImg from '../../assets/emailImg.svg'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import api from "../../services/api";
 import { parseJwt } from "../../services/auth";
@@ -11,81 +11,80 @@ import { useHistory } from "react-router-dom";
 
 
 export default function Login() {
-  
+
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
-  
+
   const [isLoading, setIsLoading] = useState(false);
-    const [erroMensagem, setErroMensagem] = useState('');
+  const [erroMensagem, setErroMensagem] = useState('');
 
-    let history = useHistory();
+  let history = useHistory();
 
-    function efetuarLogin(event) {
+  function efetuarLogin(event) {
 
-        event.preventDefault();
+    event.preventDefault();
 
-        setErroMensagem('')
-        setIsLoading(true)
+    setErroMensagem('')
+    setIsLoading(true)
 
-        api.post('/Login', {
-            email: email,
-            senha: senha
-        })
+    api.post('/Login', {
+      email: email,
+      senha: senha
+    })
 
-            .then((response) => {
-                if (response.status === 200) {
-                    localStorage.setItem('usuario-login', response.data.token)
+      .then((response) => {
+        if (response.status === 200) {
+          localStorage.setItem('usuario-login', response.data.token)
 
-                    setSenha('')
+          setSenha('')
 
-                    setEmail('')
+          setEmail('')
 
-                    setIsLoading(false)
+          setIsLoading(false)
 
-                    if (parseJwt().role === "1") {
-                        history.push('/Veiculos')
-                    }
-                    if (parseJwt().role === "2") {
-                        history.push('/Veiculos')
-                    }
-                    
-                }
-            })
-            .catch(erro => {
-                console.log(erro)
+          if (parseJwt().role === "1") {
+            history.push('/Veiculos')
+            console.log("Logado como role 1")
+          }
+          if (parseJwt().role === "2") {
+            history.push("Logado como role 2")
+          }
 
-                // setSenha('')
+        }
+      })
+      .catch(erro => {
+        console.log(erro)
 
-                setErroMensagem("E-mail e/ou Senha inválidos")
+        setErroMensagem("E-mail e/ou Senha inválidos")
 
-                setIsLoading(false)
-            })
-    }
+        setIsLoading(false)
+      })
+  }
 
   return (
     <div className="App">
       <div className="background">
         <div className="cardLogin">
-          <img src={logo} alt="Logo do loggex"className="logoLogin"/>
+          <img src={logo} alt="Logo do loggex" className="logoLogin" />
           <h1>Acesse sua conta</h1>
           <form className="formLogin" onSubmit={efetuarLogin}>
             <div className="campoLogin">
               <img src={emailImg} alt='Ilustração de email'></img>
-              <input className="inputLogin" type="email"placeholder="Email" value={email}  onChange={(e) => setEmail(e.target.value)}></input>
-              
+              <input className="inputLogin" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+
             </div>
             <div className="campoLogin">
               <img src={password} alt='Ilustração de senha'></img>
-              <input  className="inputLogin" type="password"placeholder="Senha" value={senha} onChange={(s) => setSenha(s.target.value)}></input>
+              <input className="inputLogin" type="password" placeholder="Senha" value={senha} onChange={(s) => setSenha(s.target.value)}></input>
             </div>
             <div className="esqueceste">
               <a href="">Esqueceu sua senha?</a>
             </div>
             <button disabled={
-                                        email === '' || senha === ''
-                                            ? 'none'
-                                            : ''
-                                    } type="submit" className='botao'>Entrar</button>
+              email === '' || senha === ''
+                ? 'none'
+                : ''
+            } type="submit" className='botao'>Entrar</button>
           </form>
         </div>
       </div>
