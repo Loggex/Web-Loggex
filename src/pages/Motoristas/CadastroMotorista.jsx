5e15b160ec551e1859bb6ceacd8871c047f89287
André <andre.aslima@hotmail.com>
2022-05-09 21:52:15 -0300

    Metodos de cadastro feitos

--- src/pages/Motoristas/CadastroMotorista.jsx
+++ src/pages/Motoristas/CadastroMotorista.jsx
@@ -1,8 +1,70 @@
 import Navbar from "../../Components/Navbar";
 import usuarioIndef from "../../assets/Ellipse 1.svg"
 import "../../assets/cadastroMotorista.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function CadastroMotorista(){
+
+    const [nomeInput, setNome] = useState('');
+    const [cpfInput, setCpf] = useState('');
+    const [cnhInput, setCnh] = useState('');
+    const [sexoInput, setSexo] = useState('');
+    const [emailInput, setEmail] = useState('');
+    const [senhaInput, setSenha] = useState('');
+    const [confirmSenhaInput, setConfirmSenha] = useState('');
+    const [celInput, setCel] = useState('');
+    const [imgPerfilInput, setImg] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function cadastrarMotorista(motorista) {
+        motorista.preventDefault();
+
+        setIsLoading(true);
+
+        if(senhaInput === confirmSenhaInput){
+
+            axios.post('http://localhost:5000/api/veiculos', {
+    
+                nome: nomeInput,
+                placa: cpfInput,
+                anoFabricacao: cnhInput,
+                sexo: sexoInput,
+                email: emailInput,
+                senha: senhaInput,
+                imgPerfil: imgPerfilInput,
+                numCelular: celInput
+    
+            }, {
+                headers: {
+                    //'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
+                    'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZHJlbWVsb0BlbWFpbC5jb20iLCJqdGkiOiIxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiMSIsIlRlbGVmb25lIjoiMTE5ODc2NTQzMjEiLCJleHAiOjE2NTIxMDM0NTcsImlzcyI6IkxvZ2dleC53ZWJBUEkiLCJhdWQiOiJMb2dnZXgud2ViQVBJIn0.s5YxTqVrgD025rlCLq4Ldhg5Fnm6jTFpyo8VYr4tvII'
+                }
+            })
+                .then(resposta => {
+                    if (resposta.status === 201) {
+                        console.log('Veiculo cadastrado!');
+    
+                        setNome('')
+                        setCpf('')
+                        setCnh('')
+                        setSexo('')
+                        setEmail('')
+                        setSenha('')
+                        setConfirmSenha('')
+                        setCel('')
+                        setIsLoading(false);
+                    }
+                })
+    
+                .catch(erro => console.log(erro), setInterval(() => {
+                    setIsLoading(false)
+                }, 5000));
+        }else{
+            console.log("Confirme a sua senha");
+        }
+    };
+
     return(
         <div className="backgroundapp">
             <Navbar></Navbar>
@@ -22,19 +84,19 @@ export default function CadastroMotorista(){
                                     <div className="formColuna">
                                         <div className="inputDiv">
                                         <h3>Nome completo</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setNome(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
                                         <h3>CPF</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setCpf(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
                                         <h3>CNH</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setCnh(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
                                         <h3>Sexo</h3>
-                                        <select name="Sexo" id="selectSexo">
+                                        <select name="Sexo" onChange={(campo) => setSexo(campo.target.value)} id="selectSexo">
                                             <option value="masculino">Masculino</option>
                                             <option value="feminino">Feminino</option>
                                             <option value="outros">Outro</option>
@@ -44,19 +106,19 @@ export default function CadastroMotorista(){
                                     <div className="formColuna">
                                         <div className="inputDiv">
                                         <h3>Email</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setEmail(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
                                         <h3>Senha</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setSenha(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
                                         <h3>Confirmar senha</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={(campo) => setConfirmSenha(campo.target.value)}/>
                                         </div>
                                         <div className="inputDiv">
-                                        <h3>Número de senha</h3>
-                                        <input type="text" />
+                                        <h3>Número de celular</h3>
+                                        <input type="text" onChange={(campo) => setCel(campo.target.value)}/>
                                         </div>
                                     </div>
                                 </div>
--- src/pages/Veiculos/CadastroVeiculos.jsx
+++ src/pages/Veiculos/CadastroVeiculos.jsx
@@ -1,7 +1,65 @@
 import Navbar from "../../Components/Navbar";
 import "../../assets/cadastroVeiculo.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 
 export default function CadastroVeiculos(){
+
+    const [tipoVeiculoInput, setTipo] = useState(0);
+    const [placaInput, setPlaca] = useState('');
+    const [anoInput, setAno] = useState(0);
+    const [seguroInput, setSeguro] = useState(false);
+    const [corInput, setCor] = useState('');
+    const [chassiInput, setChassi] = useState('');
+    const [quilometragemInput, setKm] = useState(0);
+    const [estadoVeiculoInput, setEstado] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function cadastrarVeiculo(veiculo) {
+        veiculo.preventDefault();
+
+        setIsLoading(true);
+
+        axios.post('http://localhost:5000/api/veiculos', {
+
+            idTipoVeiculo: tipoVeiculoInput,
+            placa: placaInput,
+            anoFabricacao: anoInput,
+            seguro: seguroInput,
+            cor: corInput,
+            chassi: chassiInput,
+            estadoVeiculo: estadoVeiculoInput,
+            quilometragem: quilometragemInput
+
+        }, {
+            headers: {
+                //'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
+                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZHJlbWVsb0BlbWFpbC5jb20iLCJqdGkiOiIxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiMSIsIlRlbGVmb25lIjoiMTE5ODc2NTQzMjEiLCJleHAiOjE2NTIxMDM0NTcsImlzcyI6IkxvZ2dleC53ZWJBUEkiLCJhdWQiOiJMb2dnZXgud2ViQVBJIn0.s5YxTqVrgD025rlCLq4Ldhg5Fnm6jTFpyo8VYr4tvII'
+            }
+        })
+            .then(resposta => {
+                if (resposta.status === 201) {
+                    console.log('Veiculo cadastrado!');
+
+                    setTipo(0)
+                    setPlaca('')
+                    setAno(0)
+                    setSeguro(false)
+                    setCor('')
+                    setChassi('')
+                    setKm(0)
+                    setEstado(false)
+                    setIsLoading(false);
+                }
+            })
+
+            .catch(erro => console.log(erro), setInterval(() => {
+                setIsLoading(false)
+            }, 5000));
+    };
+
     return(
         <div className="backgroundapp">
             <Navbar></Navbar>
@@ -9,20 +67,20 @@ export default function CadastroVeiculos(){
                 <div className="container">
                     <div className="fundoCadastroV">
                         <h1>Cadastrar um veículo</h1>
-                            <form action="" className="formCadastroV">
+                            <form action="" onSubmit={cadastrarVeiculo}  className="formCadastroV">
                                 <div className="formulariosV">
                                     <div className="formColunaV">
                                         <div className="inputDivV">
                                         <h3>Placa</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={ (campo) => setTipo(campo.target.value) } />
                                         </div>
                                         <div className="inputDivV">
                                         <h3>Cor</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={ (campo) => setCor(campo.target.value) }/>
                                         </div>
                                         <div className="inputDivV">
                                         <h3>Ano de Fabricação</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={ (campo) => setAno(campo.target.value) } />
                                         </div>
                                         <div className="inputDivV">
                                             <h3>Modelo do veículo</h3>
@@ -32,29 +90,29 @@ export default function CadastroVeiculos(){
                                     <div className="formColunaV">
                                         <div className="inputDivV">
                                         <h3>Tem seguro?</h3>
-                                        <select name="" id="selectSeguro">
+                                        <select name="" onChange={ (campo) => setSeguro(campo.target.value) } id="selectSeguro">
                                             <option value="true">Sim</option>
                                             <option value="false">Não</option>
                                             </select>
                                         </div>
                                         <div className="inputDivV">
                                         <h3>Chassi</h3>
-                                        <input type="text" />
+                                        <input type="text" onChange={ (campo) => setChassi(campo.target.value) }/>
                                         </div>
                                         <div className="inputDivV">
                                         <h3>Quilometragem</h3>
-                                        <input type="text" />
+                                        <input type="text"  onChange={ (campo) => setKm(campo.target.value) }/>
                                         </div>
                                         <div className="inputDivV" id="estadoVeiculo">
                                             <h3>Estado do Veículo</h3>
-                                            <select name="estadoVeiculo" id="">
+                                            <select name="estadoVeiculo" onChange={ (campo) => setEstado(campo.target.value) } id="">
                                                 <option value="true">Funcional</option>
                                                 <option value="false">Quebrado</option>
                                             </select>
                                         </div>
                                     </div>
                                 </div>
-                                <input value="Cadastrar" type="submit" name="cadastrar" id="botaoCadastrar" />
+                                <button value="Cadastrar" type="submit" name="cadastrar" id="botaoCadastrar">Cadastrar</button>
                             </form>
                             
                                
